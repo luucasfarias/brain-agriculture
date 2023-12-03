7958b497dcd81aefecce4ccbee6c0ca6e4923a25
lucas.farias <luucasfarias21@gmail.com>
2023-12-03 18:02:58 -0300

    (feat): Add toast container for message

--- src/app/(brain)/layout.tsx
+++ src/app/(brain)/layout.tsx
@@ -1,10 +1,16 @@
 import { Header } from "@/components/header";
 import { ReactNode } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function BrainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content]">
       <Header />
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+      />
       {children}
     </div>
   )
